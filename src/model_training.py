2f77652f8d7d9b8fd7b738f1a709a0b4731de969
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-08-28 20:14:01 +0300

    Added GridSearch

--- src/model_training.py
+++ src/model_training.py
@@ -10,16 +10,12 @@ import argparse
 
 import numpy as np
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import NearestCentroid
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 
 def parse_data_corpus(file):
     labels = []
@@ -40,40 +36,58 @@ def build_model_for_corpus(file):
     print("Fetched %d documents with %d labels\n" % 
           (len(data_train["data"]), len(data_train["target"])))
     
-    vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(1,5), max_df=0.25, 
-                                 stop_words='english', max_features=40000)
-    #vectorizer = CountVectorizer(stop_words='english')
-    #vectorizer = HashingVectorizer(stop_words='english',  n_features=2 ** 16)
-    X_train = vectorizer.fit_transform(data_train["data"])
-    print("n_samples: %d, n_features: %d" % X_train.shape)
-    
-    scaler = StandardScaler(with_mean=False)
-    X_train = scaler.fit_transform(X_train)
-    
+    X_train = data_train["data"]
     y_train = np.array(data_train["target"], dtype=int)
-    print(y_train.shape)
     
-    #clf = SGDClassifier(penalty='l1', alpha=1e-4, n_iter=10)#alpha=.0001, n_iter=50, penalty="elasticnet")
-    #clf = KNeighborsClassifier(n_neighbors=10)
-    #clf = Perceptron(n_iter=50)#PassiveAggressiveClassifier(n_iter=50)
-    clf = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, n_iter=80, random_state=42)
+    clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
+                      ('tfidf', TfidfTransformer()),
+                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                            alpha=1e-3, random_state=42,
+                                            n_iter=5)),
+                    ])
     
     clf.fit(X_train, y_train)
     
-    return vectorizer, scaler, clf
+    return clf
     
-def predict_from_input(vectorizer, scaler, clf):
+def predict_from_input(clf):
     N = 4
-    test = ["this is a document", "this is another document", "documents are seperated by newlines", "champion products ch approves stock split champion products inc said its board of directors approved a two for one stock split of its common shares for shareholders of record as of april the company also said its board voted to recommend to shareholders at the annual meeting april an increase in the authorized capital stock from five mln to mln shares reuter"]
-    #test = np.array([input_str for _ in range(N)], str)
-    X_test = vectorizer.transform(test)
-    print("n_samples: %d, n_features: %d" % X_test.shape)
-    X_test = scaler.fit_transform(X_test)
+    X_test = ["this is a document", "this is another document", "documents are seperated by newlines", 
+              "champion products ch approves stock split champion products inc said its board of directors approved a two for one stock split of its common shares for shareholders of record as of april the company also said its board voted to recommend to shareholders at the annual meeting april an increase in the authorized capital stock from five mln to mln shares reuter"]
     
     lbls = clf.predict(X_test)
     for i in range(N):
         print(lbls[i])
     
+def search_optimal_parameters(file):
+    data_train = parse_data_corpus(file)
+    print("Fetched %d documents with %d labels\n" % 
+          (len(data_train["data"]), len(data_train["target"])))
+    
+    X = data_train["data"]
+    y = np.array(data_train["target"], dtype=int)
+    
+    clf = Pipeline([('vect', CountVectorizer(stop_words='english')),
+                      ('tfidf', TfidfTransformer()),
+                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                            alpha=1e-3, random_state=42, 
+                                            n_iter=5)),
+                    ])
+    parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
+                  'vect__analyzer': ['word', 'char', 'char_wb'],
+                  'tfidf__use_idf': (True, False),
+                  'clf__alpha': (1e-3, 1e-4),
+                 }
+    gs_clf = GridSearchCV(clf, parameters, n_jobs=-1)
+    gs_clf = gs_clf.fit(X, y)
+    
+    
+    print("Best score: %.2f" % (gs_clf.best_score_))
+    
+    for param_name in sorted(parameters.keys()):
+        print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
+        
+    return gs_clf
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -82,6 +96,6 @@ if __name__ == '__main__':
     
     print("Building model for %s" % (args.corpora))
     
-    vectorizer, scaler, clf = build_model_for_corpus(args.corpora)
+    clf = search_optimal_parameters(args.corpora)
     
-    predict_from_input(vectorizer, scaler, clf)
+    predict_from_input(clf)
